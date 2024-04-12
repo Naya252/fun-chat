e6f3071082e7264cc0a9224d62fe5d383d64eb3a
Naya252 <naya252naya@gmail.com>
2024-04-13 01:48:28 +0300

    feat: add footer

---
+++ src/components/footer/footer-component.ts
@@ -0,0 +1,21 @@
+import BaseComponent from '@/components/shared/base-component';
+import styles from '@/components/footer/footer.module.css';
+
+export default class Footer extends BaseComponent {
+  constructor() {
+    super('footer', ['w-full', 'border-t', 'border-slate-700']);
+
+    const container = new BaseComponent('div', ['mx-auto', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8']);
+    const subcontainer = new BaseComponent('div', ['relative', 'flex', 'h-16', 'items-center', 'justify-between']);
+
+    const github = new BaseComponent('a', [styles.github], { href: 'https://github.com/Naya252', target: '_blank' });
+    const rss = new BaseComponent('a', [styles['rs-logo']], {
+      href: 'https://rs.school/',
+      target: '_blank',
+    });
+
+    container.append(subcontainer);
+    subcontainer.append(github, rss);
+    this.append(container);
+  }
+}
