faf7ff8bee6e8bc5a7914ea69f153d05ed597e54
Naya252 <naya252naya@gmail.com>
2024-04-13 16:26:41 +0300

    fix: change initial anchors

--- src/components/footer/footer-component.ts
+++ src/components/footer/footer-component.ts
@@ -8,8 +8,11 @@ export default class Footer extends BaseComponent {
     const container = new BaseComponent('div', ['mx-auto', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8']);
     const subcontainer = new BaseComponent('div', ['relative', 'flex', 'h-16', 'items-center', 'justify-between']);
 
-    const github = new BaseComponent('a', [styles.github], { href: 'https://github.com/Naya252', target: '_blank' });
-    const rss = new BaseComponent('a', [styles['rs-logo']], {
+    const github = new BaseComponent<HTMLAnchorElement>('a', [styles.github], {
+      href: 'https://github.com/Naya252',
+      target: '_blank',
+    });
+    const rss = new BaseComponent<HTMLAnchorElement>('a', [styles['rs-logo']], {
       href: 'https://rs.school/',
       target: '_blank',
     });
