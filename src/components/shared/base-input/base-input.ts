d38149127f270efe6ca4caacbeae0b33fdabbddc
Naya252 <naya252naya@gmail.com>
2024-04-14 02:50:28 +0300

    feat: add base input

---
+++ src/components/shared/base-input/base-input.ts
@@ -0,0 +1,135 @@
+import BaseComponent from '@/components/shared/base-component';
+
+type InputType =
+  | 'text'
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'time'
+  | 'url'
+  | 'week';
+
+export default class BaseInput extends BaseComponent {
+  private readonly input: BaseComponent;
+
+  private readonly text: BaseComponent;
+
+  constructor(
+    inputId: string,
+    labelText: string,
+    textId: string,
+    subtext: string,
+    attr: Record<string, string>,
+    inputType: InputType = 'text',
+    classes = '',
+  ) {
+    super('div', ['input-wrapper']);
+
+    const label = new BaseComponent<HTMLLabelElement>(
+      'label',
+      ['block', 'text-sm', 'font-medium', 'leading-6', 'text-gray-900', 'dark:text-gray-400'],
+      { for: inputId } as Record<string, string>,
+      labelText,
+    );
+    this.input = new BaseComponent<HTMLInputElement>(
+      'input',
+      [
+        'block',
+        'w-full',
+        'rounded-md',
+        'border-0',
+        'py-1.5',
+        'px-3',
+        'text-slate-700',
+        'dark:text-slate-200',
+        'shadow-sm',
+        'ring-1',
+        'ring-gray-500',
+        'placeholder:text-slate-700',
+        'dark:placeholder:text-slate-200',
+        'focus:outline-none',
+        'focus:ring-inset',
+        'focus:ring-2',
+        'focus:ring-sky-500',
+        'sm:text-sm',
+        'sm:leading-6',
+        'bg-gray-700',
+      ],
+      {
+        ...attr,
+        id: inputId,
+        type: inputType,
+      },
+    );
+    if (classes.length > 0) {
+      this.input.setClasses([classes]);
+    }
+    const inputWrapper = new BaseComponent('div');
+    this.text = new BaseComponent(
+      'div',
+      [
+        'form-text',
+        'text-gray-900',
+        'dark:text-gray-300',
+        'text-xs',
+        'text-rose-950',
+        'dark:text-rose-300',
+        'min-h-4',
+        'mt-1',
+      ],
+      { id: textId },
+      subtext,
+    );
+    inputWrapper.append(this.input);
+    this.append(label, inputWrapper, this.text);
+  }
+
+  public inputListener(eventName: string, listener: EventListenerOrEventListenerObject): void {
+    this.input.addListener(eventName, listener);
+  }
+
+  public getValue(): string {
+    const el = this.input.getElement();
+
+    if (!('value' in el)) {
+      throw new Error('not value');
+    }
+    if (typeof el.value !== 'string') {
+      throw new Error('not string');
+    }
+
+    return el.value;
+  }
+
+  public changeValue(value: string): void {
+    const el = this.input.getElement();
+
+    if (!('value' in el)) {
+      throw new Error('not value');
+    }
+    if (typeof value !== 'string') {
+      throw new Error('not string');
+    }
+
+    el.value = value;
+  }
+
+  public changeSubText(): void {
+    this.text.setHTML('');
+  }
+}
