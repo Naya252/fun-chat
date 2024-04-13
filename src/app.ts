06dab2d0fce8864d309eeb9ce00f17e9ada745e3
Naya252 <naya252naya@gmail.com>
2024-04-13 22:42:11 +0300

    feat: implement router

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import BaseComponent from './components/shared/base-component';
 import Header from './components/header/header-component';
 import Footer from './components/footer/footer-component';
 
+import AppRouter from './router/router';
+
 export default class App {
+  private readonly router: AppRouter;
+
   private readonly appContainer: BaseComponent;
 
   private readonly header: Header;
@@ -22,9 +26,9 @@ export default class App {
       'bg-gray-800',
     ]);
 
-    this.header = new Header();
-    this.main = new BaseComponent('div', ['content', 'container', 'flex', 'grow-[1]'], {}, 'gtwergherg');
-
+    this.main = new BaseComponent('div', ['w-full', 'flex', 'grow-[1]']);
+    this.router = new AppRouter(this.main);
+    this.header = new Header(this.router);
     this.footer = new Footer();
 
     this.appContainer.append(this.header, this.main, this.footer);
@@ -35,5 +39,9 @@ export default class App {
 
     body.className = 'antialiased text-slate-500 dark:text-slate-400';
     this.appContainer.appendToParent(body);
+
+    this.router.push('');
+    const activeRoute = this.router.getActiveRoute();
+    this.header.changeActiveLink(activeRoute);
   }
 }
--- src/components/header/header-component.ts
+++ src/components/header/header-component.ts
@@ -1,4 +1,6 @@
 import BaseComponent from '@/components/shared/base-component';
+import AppRouter from '@/router/router';
+import { ROUTES } from '@/router/pathes';
 
 const createTitle = (): BaseComponent => {
   const title = new BaseComponent('h1', [], {}, 'Fun chat');
@@ -8,16 +10,79 @@ const createTitle = (): BaseComponent => {
 };
 
 export default class Header extends BaseComponent {
-  constructor() {
-    super('nav', ['sticky', 'top-0', 'z-40', 'w-full', 'backdrop-blur', 'border-b', 'border-slate-700']);
+  router: AppRouter;
+  links: BaseComponent[];
 
+  constructor(router: AppRouter) {
+    super('nav', ['sticky', 'top-0', 'z-40', 'w-full', 'backdrop-blur', 'border-b', 'border-slate-700']);
+    this.router = router;
     const container = new BaseComponent('div', ['mx-auto', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8']);
     const subcontainer = new BaseComponent('div', ['relative', 'flex', 'h-12', 'items-center', 'justify-between']);
 
     const title = createTitle();
+    this.links = this.createLinks();
+    const linksWrapper = this.createNav();
 
     container.append(subcontainer);
-    subcontainer.append(title);
+    subcontainer.append(title, linksWrapper);
     this.append(container);
+  }
+
+  private createNav(): BaseComponent {
+    const container = new BaseComponent('nav', [
+      'text-sm',
+      'leading-6',
+      'font-semibold',
+      'text-slate-700',
+      'dark:text-slate-200',
+    ]);
+    const ul = new BaseComponent('ul', ['flex', 'space-x-8']);
+    ul.append(...this.links);
+    container.append(ul);
+
+    return container;
+  }
+
+  private createLinks(): BaseComponent[] {
+    const links = Object.entries(ROUTES).map(([name, route]) => {
+      const linkWrapper = new BaseComponent<HTMLUListElement>('li');
+      const link = new BaseComponent<HTMLAnchorElement>(
+        'a',
+        ['nav-link', 'hover:text-sky-500', 'dark:hover:text-sky-400'],
+        { id: route, href: route },
+        name,
+      );
+
+      link.addListener('click', (event) => {
+        event.preventDefault();
+        const active = this.router.getActiveRoute();
+
+        if (active !== route) {
+          this.router.push(route);
+          this.changeActiveLink(this.router.getActiveRoute());
+        }
+      });
+
+      linkWrapper.append(link);
+
+      return linkWrapper;
+    });
+    return links;
+  }
+
+  public changeActiveLink(title: string): void {
+    this.links.forEach((el) => {
+      const child = el.getFirstChild();
+      const childTitle = child.textContent;
+      if (child instanceof HTMLElement) {
+        child.classList.remove('text-sky-500');
+        child.classList.remove('dark:text-sky-400');
+
+        if (childTitle?.toLowerCase() === title) {
+          child.classList.add('text-sky-500');
+          child.classList.add('dark:text-sky-400');
+        }
+      }
+    });
   }
 }
