8db90e1f73a352913c5818aa55c790582e6ecceb
Naya252 <naya252naya@gmail.com>
2024-04-14 23:53:47 +0300

    feat: add ws service

---
+++ src/services/service.ts
@@ -0,0 +1,93 @@
+import alerts from '@/components/alert/alert';
+import store from '@/store/store';
+import emitter from '@/utils/event-emitter';
+import { isAuth, isError, isResponse } from '@/repositories/validation';
+import { USER_DICTIONARY, ERROR_TYPE } from '../types/api-types';
+
+// export const loginExternalUser = (login: string, isLogined: boolean): void => {
+//   ws.send(
+//     JSON.stringify({
+//       id: null,
+//       type: USER_DICTIONARY.externalLogin,
+//       payload: {
+//         user: {
+//           login,
+//           isLogined,
+//         },
+//       },
+//     }),
+//   );
+// };
+
+// export const logoutExternalUser = (login: string, isLogined: boolean): void => {
+//   ws.send(
+//     JSON.stringify({
+//       id: null,
+//       type: USER_DICTIONARY.externalLogout,
+//       payload: {
+//         user: {
+//           login,
+//           isLogined,
+//         },
+//       },
+//     }),
+//   );
+// };
+
+// export const deliverMessage = (isDelivered: boolean): void => {
+//   ws.send(
+//     JSON.stringify({
+//       id: 'string',
+//       type: MESSAGE_DICTIONARY.deliver,
+//       payload: {
+//         message: {
+//           id: 'string',
+//           status: {
+//             isDelivered,
+//           },
+//         },
+//       },
+//     }),
+//   );
+// };
+
+const changeAuth = (data: Record<string, string> | Record<string, Record<string, string>>): void => {
+  if (!isAuth(data)) {
+    throw new Error('payload is not auth');
+  }
+  store.user.setAuth(data.user.isLogined);
+  const isAuthUser = store.user.isAuth();
+  if (isAuthUser) {
+    emitter.emit('login');
+  }
+};
+
+export const changeError = (data: Record<string, string> | Record<string, Record<string, string>>): void => {
+  if (!isError(data)) {
+    throw new Error('payload is not error');
+  }
+  emitter.emit('loginError');
+  alerts.addAlert('warning', data.error);
+};
+
+export const callMessages = (data: string): void => {
+  const result: unknown = JSON.parse(data);
+
+  if (!isResponse(result)) {
+    throw new Error('result is not correct');
+  }
+
+  console.log(JSON.parse(data));
+
+  const { type, payload } = result;
+
+  const dictionary = {
+    [ERROR_TYPE]: changeError,
+    [USER_DICTIONARY.login]: changeAuth,
+  };
+
+  const fn = dictionary[type];
+  if (typeof fn === 'function') {
+    fn(payload);
+  }
+};
