bba69e9fbac253e33e3c0b5945c198d076744977
Naya252 <naya252naya@gmail.com>
2024-04-15 22:25:07 +0300

    feat: add logout logic

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,6 +1,7 @@
 import alerts from '@/components/alert/alert';
 import store from '@/store/store';
 import emitter from '@/utils/event-emitter';
+import { setUser } from '@/repositories/user-repository';
 import { isAuth, isError, isResponse } from '@/repositories/validation';
 import { USER_DICTIONARY, ERROR_TYPE } from '../types/api-types';
 
@@ -55,10 +56,20 @@ const changeAuth = (data: Record<string, string> | Record<string, Record<string,
   if (!isAuth(data)) {
     throw new Error('payload is not auth');
   }
+
   store.user.setAuth(data.user.isLogined);
   const isAuthUser = store.user.isAuth();
+
   if (isAuthUser) {
+    const user = store.user.getUser();
+    setUser(JSON.stringify(user));
     emitter.emit('login');
+  } else {
+    store.user.setLogin('');
+    store.user.setPassword('');
+    const user = store.user.getUser();
+    setUser(JSON.stringify(user));
+    emitter.emit('logout');
   }
 };
 
@@ -84,6 +95,7 @@ export const callMessages = (data: string): void => {
   const dictionary = {
     [ERROR_TYPE]: changeError,
     [USER_DICTIONARY.login]: changeAuth,
+    [USER_DICTIONARY.logout]: changeAuth,
   };
 
   const fn = dictionary[type];
