f07735ce08b8586869e3aca13fb23337a2d5d897
Naya252 <naya252naya@gmail.com>
2024-04-15 22:25:37 +0300

    feat: add empty pages

---
+++ src/components/info/info-page.ts
@@ -0,0 +1,14 @@
+import BaseComponent from '@/components/shared/base-component';
+
+class ErrorPage extends BaseComponent {
+  constructor() {
+    super('div', ['mx-auto', 'container', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8'], {}, 'Info');
+  }
+}
+
+const createPage = (): BaseComponent => {
+  const page = new ErrorPage();
+  return page;
+};
+
+export default createPage;
