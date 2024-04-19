6ba16cc02f84166de946d6cb192feb6dbfa701a1
Naya252 <naya252naya@gmail.com>
2024-04-20 00:43:20 +0300

    feat: add helper

---
+++ src/components/chat/service/helper.ts
@@ -0,0 +1,23 @@
+export const formatDate = (datetime: number): string => {
+  const currentDate = new Date(datetime);
+
+  const formattedDate = currentDate.toLocaleDateString('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
+  const formattedTime = currentDate.toLocaleTimeString('ru-RU', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  const formattedDateTime = `${formattedDate}, ${formattedTime}`;
+
+  return formattedDateTime;
+};
+
+export const scrollTo = (element: HTMLElement): void => {
+  element.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+};
