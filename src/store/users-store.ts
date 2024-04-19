29599a2f2c2f26a08126b1cdc2f3871196bee78f
Naya252 <naya252naya@gmail.com>
2024-04-19 20:48:41 +0300

    feat: save users messages

--- src/store/users-store.ts
+++ src/store/users-store.ts
@@ -1,28 +1,51 @@
-import { type Member } from '@/types/api-types';
+import type { Member, Message } from '@/types/api-types';
+import emitter from '@/utils/event-emitter';
 
 export default class Users {
   private activeUsers: Member[];
   private inactiveUsers: Member[];
+  private isGetActive: boolean;
+  private isGetInactive: boolean;
+  private currentUser: string;
 
   constructor() {
     this.activeUsers = [];
     this.inactiveUsers = [];
+    this.isGetActive = false;
+    this.isGetInactive = false;
+    this.currentUser = '';
+  }
+
+  private setCurrentUser(currentUser: string): void {
+    this.currentUser = currentUser;
   }
 
   public getActiveUsers(): Member[] {
     return this.activeUsers;
   }
 
-  public setActiveUsers(activeUsers: Member[]): void {
-    this.activeUsers = [...activeUsers];
+  public setActiveUsers(activeUsers: Member[], currentUser: string): void {
+    this.activeUsers = [...activeUsers.map((user) => ({ ...user, messages: [], newMessages: [] }))];
+    this.isGetActive = true;
+    this.setCurrentUser(currentUser);
+    this.checkUsers();
   }
 
   public getInactiveUsers(): Member[] {
     return this.activeUsers;
   }
 
-  public setInactiveUsers(inactiveUsers: Member[]): void {
-    this.inactiveUsers = [...inactiveUsers];
+  public setInactiveUsers(inactiveUsers: Member[], currentUser: string): void {
+    this.inactiveUsers = [...inactiveUsers.map((user) => ({ ...user, messages: [], newMessages: [] }))];
+    this.isGetInactive = true;
+    this.setCurrentUser(currentUser);
+    this.checkUsers();
+  }
+
+  private checkUsers(): void {
+    if (this.isGetActive && this.isGetInactive) {
+      this.getHistory();
+    }
   }
 
   public getUsers(): Member[] {
@@ -35,7 +58,56 @@ export default class Users {
     if (item !== undefined) {
       item.isLogined = user.isLogined;
     } else {
-      this.activeUsers.push(user);
+      this.activeUsers.push({ ...user, messages: [], newMessages: [] });
     }
+  }
+
+  private getHistory(): void {
+    const allUsers = this.getUsers();
+
+    allUsers.forEach((user) => {
+      if (user.login !== this.currentUser) {
+        emitter.emit('get-user-history', user.login);
+      }
+    });
+
+    emitter.emit('change-users');
+  }
+
+  public setMessages(message: Message): void {
+    const { from, to } = message;
+    const member = [from, to].filter((login) => login !== this.currentUser);
+
+    const memberObj = this.getUsers().find((user) => user.login === member[0]);
+
+    if (
+      memberObj !== undefined &&
+      'messages' in memberObj &&
+      memberObj.messages instanceof Array &&
+      'newMessages' in memberObj &&
+      memberObj.newMessages instanceof Array
+    ) {
+      memberObj.messages.push(message);
+
+      if (message.from === member[0]) {
+        memberObj.newMessages.push(message);
+        emitter.emit('change-users');
+        console.log('FIX ME', memberObj);
+      }
+    }
+
+    emitter.emit('get-message', message);
+  }
+
+  public changeHistory(messages: Message[]): void {
+    messages.forEach((message) => {
+      this.setMessages(message);
+    });
+  }
+
+  public getChatData(member: string): Member | undefined {
+    const data = this.getUsers().find((user) => user.login === member);
+
+    return data;
   }
 }
