55fff8a0942ee42d1a5a69312207ad84d75b2be9
Naya252 <naya252naya@gmail.com>
2024-04-20 18:22:03 +0300

    feat: add methods in store for change message status

--- src/store/users-store.ts
+++ src/store/users-store.ts
@@ -1,6 +1,50 @@
 import type { Member, Message } from '@/types/api-types';
 import emitter from '@/utils/event-emitter';
 
+const setReadSatus = (msg: Message, isReaded: boolean | null): void => {
+  if (isReaded === null) {
+    return;
+  }
+
+  const copyMsg = msg;
+  copyMsg.status.isReaded = isReaded;
+};
+
+const setDelideredSatus = (msg: Message, isDelivered: boolean | null): void => {
+  if (isDelivered === null) {
+    return;
+  }
+
+  const copyMsg = msg;
+  copyMsg.status.isDelivered = isDelivered;
+};
+
+const setEditedSatus = (msg: Message, isEdited: boolean | null): void => {
+  if (isEdited === null) {
+    return;
+  }
+
+  const copyMsg = msg;
+  copyMsg.status.isEdited = isEdited;
+};
+
+const setNewMessages = (user: Member, id: string): void => {
+  const copyUser = user;
+  if (copyUser.newMessages instanceof Array) {
+    copyUser.newMessages = copyUser.newMessages.filter((el) => el.id !== id);
+    emitter.emit('change-counter', user.login);
+  }
+};
+
+const cleanFirstNewMessage = (id: string, user: Member): void => {
+  const copy = user;
+  if (copy.firstNewMessage === id) {
+    emitter.emit('clean-divider', { member: user.login, idMsg: id });
+
+    copy.firstNewMessage = '';
+  }
+};
+
 export default class Users {
   private activeUsers: Member[];
   private inactiveUsers: Member[];
@@ -74,8 +118,6 @@ export default class Users {
         emitter.emit('get-user-history', user.login);
       }
     });
-
-    emitter.emit('change-users');
   }
 
   public setMessages(message: Message): void {
@@ -99,20 +141,44 @@ export default class Users {
         }
         memberObj.newMessages.push(message);
 
-        console.log(memberObj);
-
-        emitter.emit('change-users');
-        console.log('FIX ME', memberObj);
+        emitter.emit('change-counter', message.from);
       }
     }
 
     emitter.emit('get-message', message);
   }
 
+  public setStatus(
+    id: string,
+    isReaded: boolean | null = null,
+    isDelivered: boolean | null = null,
+    isEdited: boolean | null = null,
+  ): void {
+    const allUsers = this.getUsers();
+    allUsers.forEach((user) => {
+      if (user.newMessages?.some((message) => message.id === id)) {
+        cleanFirstNewMessage(id, user);
+        const msg = user.newMessages.find((el) => el.id === id);
+
+        if (msg !== undefined) {
+          setReadSatus(msg, isReaded);
+          setDelideredSatus(msg, isDelivered);
+          setEditedSatus(msg, isEdited);
+          setNewMessages(user, msg.id);
+        }
+
+        console.log('CHANGE MSG - msg');
+
+        console.log(user);
+      }
+    });
+  }
+
   public changeHistory(messages: Message[]): void {
     messages.forEach((message) => {
       this.setMessages(message);
     });
+    emitter.emit('change-users');
   }
 
   public getChatData(member: string): Member | undefined {
