02e954396866d25119de6406fc3a2aefc2c3d87f
Naya252 <naya252naya@gmail.com>
2024-04-21 03:33:18 +0300

    feat: add helper for choose actions position

--- src/components/chat/service/helper.ts
+++ src/components/chat/service/helper.ts
@@ -35,3 +35,43 @@ export const readMessages = (member: string): void => {
     });
   }
 };
+
+export const calculateActionsPosition = (event: Event, chatCard: HTMLElement): Record<string, string> => {
+  let left = '0';
+  let top = '0';
+  let right = `${null}`;
+  let bottom = `${null}`;
+
+  if (event instanceof MouseEvent) {
+    left = `${event.layerX}px`;
+    right = `${null}`;
+    top = `${event.layerY}px`;
+    bottom = `${null}`;
+
+    const width = 128;
+    const height = 80;
+    const chat = chatCard.getBoundingClientRect();
+
+    if (event.pageX + width > Math.ceil(chat.right)) {
+      left = `${null}`;
+      right = '0';
+    }
+
+    if (event.pageX < Math.ceil(chat.left)) {
+      left = '0';
+      right = `${null}`;
+    }
+
+    if (event.pageY + height > Math.ceil(chat.top)) {
+      top = '0';
+      bottom = `${null}`;
+    }
+
+    if (event.pageY + height < Math.ceil(chat.bottom)) {
+      top = `${null}`;
+      bottom = '0';
+    }
+  }
+
+  return { left, right, top, bottom };
+};
