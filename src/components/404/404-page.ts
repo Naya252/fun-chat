05176a03d82c849f845c325bf94e98cd9fbe2573
Naya252 <naya252naya@gmail.com>
2024-04-22 04:30:36 +0300

    feat: add info, error pages

--- src/components/404/404-page.ts
+++ src/components/404/404-page.ts
@@ -2,7 +2,25 @@ import BaseComponent from '@/components/shared/base-component';
 
 class ErrorPage extends BaseComponent {
   constructor() {
-    super('div', ['mx-auto', 'container', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8'], {}, '404');
+    super('div', ['mx-auto', 'container', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8', 'select-none']);
+
+    const card = new BaseComponent('div', ['text-center', 'py-24', 'sm:py-32', 'lg:px-8']);
+    const info = new BaseComponent('p', ['text-base', 'font-semibold', 'text-sky-500'], {}, '404');
+    const title = new BaseComponent(
+      'h1',
+      ['mt-4', 'text-3xl', 'font-bold', 'tracking-tight', 'text-gray-300', 'sm:text-5xl'],
+      {},
+      'Page not found',
+    );
+    const text = new BaseComponent(
+      'p',
+      ['mt-6', 'text-base', 'leading-7', 'text-gray-400'],
+      {},
+      'Sorry, we couldn’t find the page you’re looking for.',
+    );
+
+    card.append(info, title, text);
+    this.append(card);
   }
 }
 
--- src/components/info/info-page.ts
+++ src/components/info/info-page.ts
@@ -1,13 +1,31 @@
 import BaseComponent from '@/components/shared/base-component';
 
-class ErrorPage extends BaseComponent {
+class InfoPage extends BaseComponent {
   constructor() {
-    super('div', ['mx-auto', 'container', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8'], {}, 'Info');
+    super('div', ['mx-auto', 'container', 'max-w-7xl', 'px-2', 'sm:px-6', 'lg:px-8', 'select-none']);
+
+    const card = new BaseComponent('div', ['text-center', 'max-w-80', 'mx-auto', 'py-24', 'sm:py-32', 'lg:px-8']);
+    const info = new BaseComponent('p', ['text-base', 'font-semibold', 'text-sky-500'], {}, 'author: Naya252');
+    const title = new BaseComponent(
+      'h1',
+      ['mt-4', 'text-3xl', 'font-bold', 'tracking-tight', 'text-gray-300', 'sm:text-5xl'],
+      {},
+      'Fun chat',
+    );
+    const text = new BaseComponent(
+      'p',
+      ['mt-6', 'text-base', 'leading-7', 'text-gray-400'],
+      {},
+      'The application is designed to demonstrate the Fun Chat task as part of the RSSchool JS/FE 2023Q4 course.',
+    );
+
+    card.append(info, title, text);
+    this.append(card);
   }
 }
 
 const createPage = (): BaseComponent => {
-  const page = new ErrorPage();
+  const page = new InfoPage();
   return page;
 };
 
